7a4c7fd5bcfb4f8492a4227ca605eb543daf133a
Yakov <yasamsonov@gmail.com>
2022-08-12 11:08:42 +0300

    Get unfura from env.local

--- pages/create-post.js
+++ pages/create-post.js
@@ -10,8 +10,25 @@ import {
   } from "../config"
   
 import Blog from '../artifacts/contracts/Blog.sol/Blog.json'
-  
-const client = create('https://ipfs.infura.io:5001/api/v0')
+
+function createIPFSClient() {
+    const projectId = process.env.INFURA_PROJECT_ID
+    const projectSecret = process.env.INFURA_PROJECT_KEY
+
+    const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+    const client = await ipfsClient.create({
+       host: 'ipfs.infura.io',
+       port: 5001,
+       protocol: 'https',
+       apiPath: '/api/v0',
+       headers: {
+         authorization: auth
+       }
+     })
+
+    return client
+}
 
 const SimpleMDE = dynamic(
     () => import('react-simplemde-editor'),
@@ -24,6 +41,12 @@ function CreatePost() {
     const [post, setPost] = useState(initialState)
     const [image, setImage] = useState(null)
     const [loaded, setLoaded] = useState(false)
+
+    const fileRef = useRef(null)
+    const { title, content } = post
+    const router = useRouter()
+
+    const client = createIPFSClient()
 
     useEffect(() => {
         setTimeout(() => {
@@ -43,6 +66,128 @@ function CreatePost() {
             console.log('error: ', err)
         }
     }
+
+    async function savePost(hash) {
+        if (typeof window.ethereum !== 'undefined') {
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const signer = provider.getSigner()
+            const contract = new ethers.Contract(contractAddress, Blog.abi, signer)
+            console.log('contract: ', contract)
+            try {
+                const val = await contract.CreatePost(post.title, hash)
+                /* optional - wait for transaction to be confirmed before rerouting */
+                /* await provider.waitForTransaction(val.hash)*/
+                console.log('val: ', val)
+            } catch (err) {
+                console.log('Error: ', err)
+            }
+        }
+    }
+
+    async function createNewPost() {
+        if (!title || !content) return
+        const hash = await savePostToIpfs()
+        await savePost(hash)
+        router.push('/')
+    }
+
+    async function handleFileChange (e) {
+        const uploadedFile = e.target.files[0]
+        if (!uploadedFile) return
+        const added = await client.add(uploadedFile)
+        setPost(state => ({ ...state, coverImage: added.path}))
+        setImage(uploadedFile)
+    }
+
+    function triggerOnChange() {
+        fileRef.current.click()
+    }
+
+    return (
+        <div className={container}>
+          {
+            image && (
+              <img className={coverImageStyle} src={URL.createObjectURL(image)} />
+            )
+          }
+          <input
+            onChange={onChange}
+            name='title'
+            placeholder='Give it a title ...'
+            value={post.title}
+            className={titleStyle}
+          />
+          <SimpleMDE
+            className={mdEditor}
+            placeholder="What's on your mind?"
+            value={post.content}
+            onChange={value => setPost({ ...post, content: value })}
+          />
+          {
+            loaded && (
+              <>
+                <button
+                  className={button}
+                  type='button'
+                  onClick={createNewPost}
+                >Publish</button>
+                <button
+                  onClick={triggerOnChange}
+                  className={button}
+                >Add cover image</button>
+              </>
+            )
+          }
+          <input
+            id='selectImage'
+            className={hiddenInput} 
+            type='file'
+            onChange={handleFileChange}
+            ref={fileRef}
+          />
+        </div>
+      )
 }
+    
+const hiddenInput = css`
+  display: none;
+`
+
+const coverImageStyle = css`
+  max-width: 800px;
+`
+
+const mdEditor = css`
+  margin-top: 40px;
+`
+
+const titleStyle = css`
+  margin-top: 40px;
+  border: none;
+  outline: none;
+  background-color: inherit;
+  font-size: 44px;
+  font-weight: 600;
+  &::placeholder {
+    color: #999999;
+  }
+`
+
+const container = css`
+  width: 800px;
+  margin: 0 auto;
+`
+
+const button = css`
+  background-color: #fafafa;
+  outline: none;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  margin-right: 10px;
+  font-size: 18px;
+  padding: 16px 70px;
+  box-shadow: 7px 7px rgba(0, 0, 0, .1);
+`
 
 export default CreatePost
